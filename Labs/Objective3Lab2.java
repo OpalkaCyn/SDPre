87ee1dc2db3e99dc37a9b42dbb789b0c3b0ae045
Cynthia Opalka <48398906+OpalkaCyn@users.noreply.github.com>
2023-09-11 14:06:01 -0600

    Objective3Lab2.java

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,9 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1, side2, hypotenuse;
+    side1 = 10;
+    side2 = 8;
+    hypotenuse = Math.sqrt(side1) + Math.sqrt(side2); 
+    System.out.println(hypotenuse);
+  } 
+}
