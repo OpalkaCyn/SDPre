7ab7513caad1b3f73458d00c9455f5202b90b4dd
Cynthia Opalka <48398906+OpalkaCyn@users.noreply.github.com>
2023-09-11 15:20:07 -0600

    Objective4Lab1.java

---
+++ Labs/Objective4Lab1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+
+    String fname = "";
+    String lname = "";
+    String favoriteAnimal = "";
+    String favoriteFood = "";
+    String favoriteSong = "";
+
+    //TODO prompt the user to get input for all of the String variables.
+    System.out.println("What is your first name?")
+    String fname = scanner.nextString();
+
+    System.out.println("What is your last name?")
+    String lname = scanner.nextString();
+
+    System.out.println("what is your favorite animal?");
+    String favoriteAnimal = scanner.nextString();
+
+    System.out.println("What is your favorite food?");
+    String favoriteFood = scanner.nextString();
+
+    System.out.printtln("What is your favorite song?");
+    String favoriteSong = scanner.nextString();
+
+    //TODO print the output formatted to look like the expected output using String concatenation.
+
+  }
+}
