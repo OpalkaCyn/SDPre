c9797ba8b37f49b9bb0d3c87f3fab12a962ff3bc
Cynthia Opalka <48398906+OpalkaCyn@users.noreply.github.com>
2023-09-11 13:52:05 -0600

    Create Objective3Lab1.java

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,11 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 5;
+    num2 = 6;
+    num3 = 7;
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println(average);
+  }
+}
