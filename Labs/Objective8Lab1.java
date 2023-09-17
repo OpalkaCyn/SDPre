0300f66a0e03ca2035b99a1b28aec1d55629d5a7
Cynthia Opalka <48398906+OpalkaCyn@users.noreply.github.com>
2023-09-17 02:17:52 +0000

    10

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,13 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    //TODO Write a for loop that will print the numbers 1 to 10
+    int num;
+    for (num = 1; num < 11; ++num) {
+    System.out.println(num);
+    }
+    //TODO Write another for loop that will print the numbers 10 to 1.
+    for (num = 10; num >= 1; --num) {
+        System.out.println(num);
+    }
+  }
+}
