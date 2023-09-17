8dfcc55be79383d87a89e7a9a4911df75ae836d8
Cynthia Opalka <48398906+OpalkaCyn@users.noreply.github.com>
2023-09-17 01:20:59 +0000

    sum

---
+++ Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab4 {
+    public static void main(String[] args){
+    int sum = 0;
+    int count = 0;
+
+        while (count <=20) {
+            sum = sum + count;
+            System.out.println(sum);
+            ++count;
+        }
+    }
+
+}
